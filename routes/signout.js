d58f38f5c3a389d553aaf5238cdf5c99cbd25271
patriciabernardos <ptrbernardos@gmail.com>
2025-05-30 18:42:42 +0200

    fix: typo and config errors in signup and signout routes and ports

--- routes/signout.js
+++ routes/signout.js
@@ -1,20 +1,6 @@
-const { json } = require('body-parser');
 const router = require('express').Router();
-
-
-router.post('/', (req, res) => {
-    const {username, password} = req.body;
-    if(!!username || !!password) {
-        return res.status(400).json(
-            jsonResponse( 400, {
-                error:"Fields are required",})
-        );
-    }
-    
-    res.status(200).json(jsonResponse(200, { message: "User created successfully"}));
+router.get('/', (req, res) => {
     res.send('signout');
 });
-
-
 
 module.exports = router;
--- routes/signup.js
+++ routes/signup.js
@@ -1,6 +1,20 @@
+const { jsonResponse } = require('../libs/jsonResponse');
 const router = require('express').Router();
-router.get('/', (req, res) => {
+
+
+router.post('/', (req, res) => {
+    const {username, password} = req.body;
+    if(!!!username || !!!password) {
+        return res.status(400).json(
+            jsonResponse( 400, {
+                error:"Fields are required",})
+        );
+    }
+    
+    res.status(200).json(jsonResponse(200, { message: "User created successfully"}));
     res.send('signup');
 });
+
+
 
 module.exports = router;
